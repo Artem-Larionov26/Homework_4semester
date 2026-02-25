c2c0a179d73ad26d57427d80e3a1c21527f025aa
homework_1sem <st129909@student.spbu.ru>
2026-02-26 00:49:43 +0300

    feat: add tests

---
+++ NumberSearch/NumberSearch.fs
@@ -0,0 +1,13 @@
+﻿module ListSearch
+
+let findFirst (value: 'a) (list: 'a list) : int option =
+    let rec loop index remaining =
+        match remaining with
+        | [] -> None
+        | head :: tail ->
+            if head = value then
+                Some index
+            else
+                loop (index + 1) tail
+
+    loop 0 list
---
+++ NumberSearchTests/NumberSearchTests.fs
@@ -0,0 +1,27 @@
+﻿module ListSearchTests
+
+open NUnit.Framework
+open ListSearch
+
+[<TestFixture>]
+type SearchTests() =
+
+    [<Test>]
+    member _.``element found at beginning``() =
+        Assert.That(findFirst 1 [1;2;3], Is.EqualTo(Some 0))
+
+    [<Test>]
+    member _.``element found in middle``() =
+        Assert.That(findFirst 2 [1;2;3], Is.EqualTo(Some 1))
+
+    [<Test>]
+    member _.``element found at end``() =
+        Assert.That(findFirst 3 [1;2;3], Is.EqualTo(Some 2))
+
+    [<Test>]
+    member _.``element not found``() =
+        Assert.That(findFirst 4 [1;2;3], Is.EqualTo(None))
+
+    [<Test>]
+    member _.``empty list``() =
+        Assert.That(findFirst 1 [], Is.EqualTo(None))

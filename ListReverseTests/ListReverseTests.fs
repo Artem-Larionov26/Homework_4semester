4a75d78b6d40a4ca7914e1214970f2aba8bfbc26
homework_1sem <st129909@student.spbu.ru>
2026-02-26 00:16:49 +0300

    feat: add tests

---
+++ ListReverseTests/ListReverseTests.fs
@@ -0,0 +1,26 @@
+﻿module ListReverseTests
+
+open NUnit.Framework
+open ListReverse
+
+[<TestFixture>]
+type ReverseTests() =
+
+    [<Test>]
+    member _.``empty list``() =
+        Assert.That(reverse [], Is.EqualTo([]))
+
+    [<Test>]
+    member _.``single element``() =
+        Assert.That(reverse [1], Is.EqualTo([1]))
+
+    [<Test>]
+    member _.``multiple elements``() =
+        Assert.That(reverse [1;2;3], Is.EqualTo([3;2;1]))
+
+    [<Test>]
+    member _.``string list``() =
+        Assert.That(
+            reverse ["a"; "b"; "c"],
+            Is.EqualTo(["c"; "b"; "a"])
+        )

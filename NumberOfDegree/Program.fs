14490122a818e2377c0b5fd60513a16cb5536090
homework_1sem <st129909@student.spbu.ru>
2026-02-26 01:25:02 +0300

    feat: add tests

---
+++ NumberOfDegree/Program.fs
@@ -0,0 +1,15 @@
+﻿module NumberOfDegree
+
+let powersOfTwo (n: int) (m: int) : bigint list =
+    if m < 0 then
+        invalidArg "m" "m must be non-negative"
+    else
+        let start = pown 2I n
+
+        let rec loop count current acc =
+            if count > m then
+                List.rev acc
+            else
+                loop (count + 1) (current * 2I) (current :: acc)
+
+        loop 0 start []
---
+++ NumberOfDegreeTests/NumberOfDegreeTests.fs
@@ -0,0 +1,32 @@
+﻿module NumberOfDegreeTests
+
+open NUnit.Framework
+open NumberOfDegree
+
+[<TestFixture>]
+type PowerSeriesTests() =
+
+    [<Test>]
+    member _.``n=0 m=0``() =
+        Assert.That(powersOfTwo 0 0, Is.EqualTo([1I]))
+
+    [<Test>]
+    member _.``n=1 m=3``() =
+        Assert.That(
+            powersOfTwo 1 3,
+            Is.EqualTo([2I; 4I; 8I; 16I])
+        )
+
+    [<Test>]
+    member _.``n=3 m=2``() =
+        Assert.That(
+            powersOfTwo 3 2,
+            Is.EqualTo([8I; 16I; 32I])
+        )
+
+    [<Test>]
+    member _.``negative m throws``() =
+        Assert.That(
+            (fun () -> powersOfTwo 2 -1 |> ignore),
+            Throws.TypeOf<System.ArgumentException>()
+        )

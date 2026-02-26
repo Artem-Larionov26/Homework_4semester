d7d4db5f75a116aa84229d0241ca55b7a614c6c8
homework_1sem <st129909@student.spbu.ru>
2026-02-26 16:43:28 +0300

    feat: add tests

---
+++ EvenNumbersTests/EvenNumbersTests.fs
@@ -0,0 +1,25 @@
+﻿module EvenNumbersTests
+
+open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+open EvenNumbers
+
+[<TestFixture>]
+type EvenNumbersTests() =
+
+    [<Test>]
+    member _.``empty list``() =
+        Assert.That(countEvenFilter [], Is.EqualTo(0))
+
+    [<Test>]
+    member _.``simple example``() =
+        Assert.That(countEvenFilter [1;2;3;4], Is.EqualTo(2))
+
+    [<Property>]
+    member _.``filter and map equivalent`` (xs: int list) =
+        countEvenFilter xs = countEvenMap xs
+
+    [<Property>]
+    member _.``filter and fold equivalent`` (xs: int list) =
+        countEvenFilter xs = countEvenFold xs

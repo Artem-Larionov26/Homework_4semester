7fd5a37d18156871e7c50c37933306b7cee33ae8
homework_1sem <st129909@student.spbu.ru>
2026-02-26 17:56:41 +0300

    feat: add tests

---
+++ TreeMap/TreeMap.fs
@@ -0,0 +1,15 @@
+﻿module TreeMap
+
+type Tree<'a> =
+    | Empty
+    | Node of 'a * Tree<'a> * Tree<'a>
+
+let rec map (f: 'a -> 'b) (tree: Tree<'a>) : Tree<'b> =
+    match tree with
+    | Empty -> Empty
+    | Node(value, left, right) ->
+        Node(
+            f value,
+            map f left,
+            map f right
+        )

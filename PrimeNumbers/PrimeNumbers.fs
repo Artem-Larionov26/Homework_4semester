b250ef24867f474de2f77aa1eb3186f4d1ece77a
homework_1sem <st129909@student.spbu.ru>
2026-02-27 01:45:02 +0300

    feat: add tests

---
+++ PrimeNumbers/PrimeNumbers.fs
@@ -0,0 +1,12 @@
+﻿module PrimeNumbers
+
+let isPrime n =
+    if n < 2 then false
+    else
+        let bound = int (sqrt (float n))
+        seq { 2 .. bound }
+        |> Seq.forall (fun d -> n % d <> 0)
+
+let primes : seq<int> =
+    Seq.initInfinite (fun i -> i + 2)
+    |> Seq.filter isPrime
---
+++ PrimeNumbersTests/PrimeNumbersTests.fs
@@ -0,0 +1,35 @@
+﻿module PrimeNumbersTests
+
+open NUnit.Framework
+open PrimeNumbers
+
+[<TestFixture>]
+type PrimeNumbersTests() =
+
+    [<Test>]
+    member _.``first prime is 2``() =
+        Assert.That(
+            primes |> Seq.head,
+            Is.EqualTo(2)
+        )
+
+    [<Test>]
+    member _.``first five primes``() =
+        Assert.That(
+            primes |> Seq.take 5 |> Seq.toList,
+            Is.EqualTo([2; 3; 5; 7; 11])
+        )
+
+    [<Test>]
+    member _.``first ten primes``() =
+        Assert.That(
+            primes |> Seq.take 10 |> Seq.toList,
+            Is.EqualTo([2; 3; 5; 7; 11; 13; 17; 19; 23; 29])
+        )
+
+    [<Test>]
+    member _.``100th prime``() =
+        Assert.That(
+            primes |> Seq.item 99,
+            Is.EqualTo(541)
+        )

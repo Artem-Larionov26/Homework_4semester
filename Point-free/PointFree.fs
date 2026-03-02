6cc347f659370da5e58ffa13f2ef87b577372371
homework_1sem <st129909@student.spbu.ru>
2026-03-02 04:00:11 +0300

    feat: add tests

---
+++ Point-free/PointFree.fs
@@ -0,0 +1,10 @@
+﻿module PointFree
+
+let funcOriginal x l =
+    List.map (fun y -> y * x) l
+
+let flip f a b =
+    f b a
+
+let func x =
+    List.map (flip (*) x)
---
+++ PountFreeTests/PointFreeTests.fs
@@ -0,0 +1,19 @@
+﻿module PointFreeTests
+
+open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+open NUnit.Framework.Legacy
+open PointFree
+
+[<TestFixture>]
+type PointFreeTests() =
+
+    [<Test>]
+    member _.``Simple example``() =
+        let result = func 3 [1;2;3]
+        CollectionAssert.AreEqual([3;6;9], result)
+
+    [<Property>]
+    member _.``Point-free version equals original`` (x:int) (l:int list) =
+        funcOriginal x l = func x l

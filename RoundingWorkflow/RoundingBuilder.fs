cca32eb7568e03ab2e60d0e2008227b7b22caa6b
homework_1sem <st129909@student.spbu.ru>
2026-03-27 23:52:48 +0300

    feat: add tests

---
+++ RoundingWorkflow/RoundingBuilder.fs
@@ -0,0 +1,18 @@
+﻿namespace RoundingWorkflow
+
+type RoundingBuilder(precision: int) =
+
+    let roundValue (x: float) =
+        System.Math.Round(x, precision)
+
+    member _.Bind(value: float, f: float -> float) =
+        value |> roundValue |> f
+
+    member _.Return(x: float) =
+        roundValue x
+
+    member _.ReturnFrom(x: float) =
+        roundValue x
+
+    member _.Zero() =
+        0.0
---
+++ RoundingWorkflowTests/RoundingWorkflowTests.fs
@@ -0,0 +1,56 @@
+﻿module RoundingWorkflowTests
+
+open NUnit.Framework
+open FsUnit
+open RoundingWorkflow.Rounding
+
+[<Test>]
+let ``Rounding works correctly`` () =
+    let result =
+        rounding 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+
+    result |> should equal 0.048
+
+[<Test>]
+let ``Intermediate rounding works`` () =
+    let result =
+        rounding 2 {
+            let! a = 10.0 / 3.0
+            let! b = 3.0
+            return a / b
+        }
+
+    result |> should equal 1.11
+
+[<Test>]
+let ``Return is rounded`` () =
+    let result =
+        rounding 2 {
+            return 1.234
+        }
+
+    result |> should equal 1.23
+
+[<Test>]
+let ``Multiple operations rounding`` () =
+    let result =
+        rounding 2 {
+            let! a = 1.0 / 3.0
+            let! b = 2.0 / 3.0
+            return a + b
+        }
+
+    result |> should equal 1.0
+
+[<Test>]
+let ``ReturnFrom works`` () =
+    let result =
+        rounding 2 {
+            return! 1.236
+        }
+
+    result |> should equal 1.24

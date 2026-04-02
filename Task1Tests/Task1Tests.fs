43bedd52c196be0557f3de61679a5413b1ba77d5
homework_1sem <st129909@student.spbu.ru>
2026-04-02 15:54:10 +0300

    feat: add tests

---
+++ Task1Tests/Task1Tests.fs
@@ -0,0 +1,22 @@
+﻿module Task1Tests
+
+open NUnit.Framework
+open FsUnit
+open Task1
+
+[<Test>]
+let ``min of list`` () =
+    minElement [3; 1; 4; 2] |> should equal 1
+
+[<Test>]
+let ``single element`` () =
+    minElement [5] |> should equal 5
+
+[<Test>]
+let ``negative numbers`` () =
+    minElement [-3; -1; -10] |> should equal -10
+
+[<Test>]
+let ``empty list throws`` () =
+    (fun () -> minElement [] |> ignore)
+    |> should throw typeof<System.Exception>

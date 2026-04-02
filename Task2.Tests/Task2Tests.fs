eb3f5454e72c6ff41379468be4327266dd4fbd29
homework_1sem <st129909@student.spbu.ru>
2026-04-02 16:13:43 +0300

    feat: add tests

---
+++ Task2.Tests/Task2Tests.fs
@@ -0,0 +1,17 @@
+﻿module Task2Tests
+
+open NUnit.Framework
+open FsUnit
+open Task2
+
+[<Test>]
+let ``square of size 4`` () =
+    printSquare 4 |> should equal "****\n*  *\n*  *\n****"
+
+[<Test>]
+let ``square of size 1`` () =
+    printSquare 1 |> should equal "*"
+
+[<Test>]
+let ``square of size 2`` () =
+    printSquare 2 |> should equal "**\n**"

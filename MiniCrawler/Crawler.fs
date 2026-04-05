558abd29a12aa537137f34e657e13b7099bcd89a
homework_1sem <st129909@student.spbu.ru>
2026-04-06 02:58:05 +0300

    feat: add tests

---
+++ MiniCrawler/Crawler.fs
@@ -0,0 +1,37 @@
+﻿module MiniCrawler.Crawler
+
+open System.Net.Http
+open MiniCrawler.Html
+
+
+let httpClient = new HttpClient()
+
+let downloadAsync (url: string) =
+    async {
+        try
+            let! html = httpClient.GetStringAsync(url) |> Async.AwaitTask
+            return Some (url, html)
+        with _ ->
+            return None
+    }
+
+let crawl url =
+    async {
+        let! mainPage = downloadAsync url
+
+        match mainPage with
+        | None -> return []
+        | Some (_, html) ->
+            let links = extractLinks html |> List.distinct
+
+            let! results =
+                links
+                |> List.map downloadAsync
+                |> Async.Parallel
+
+            return
+                results
+                |> Array.choose id
+                |> Array.map (fun (url, html) -> (url, html.Length))
+                |> Array.toList
+    }
---
+++ MiniCrawlerTests/MiniCrawlerTests.fs
@@ -0,0 +1,34 @@
+﻿module MiniCrawlerTests
+
+open NUnit.Framework
+open MiniCrawler.Html
+
+[<Test>]
+let ``Extract single link`` () =
+    let html = "<a href=\"http://a.com\">A</a>"
+    let result = extractLinks html
+    Assert.That(result, Is.EqualTo(["http://a.com"]))
+
+[<Test>]
+let ``Extract multiple links`` () =
+    let html = "<a href=\"http://a.com\">A</a><a href=\"http://b.com\">B</a>"
+    let result = extractLinks html
+    Assert.That(result, Is.EqualTo(["http://a.com"; "http://b.com"]))
+
+[<Test>]
+let ``Ignore non-http links`` () =
+    let html = "<a href=\"https://a.com\">A</a>"
+    let result = extractLinks html
+    Assert.That(result, Is.Empty)
+
+[<Test>]
+let ``No links returns empty list`` () =
+    let html = "<html></html>"
+    let result = extractLinks html
+    Assert.That(result, Is.Empty)
+
+[<Test>]
+let ``Handles broken html safely`` () =
+    let html = "<a href=\"http://a.com\">"
+    let result = extractLinks html
+    Assert.That(result, Is.EqualTo(["http://a.com"]))

147990302a5679190f3d2ce5fef262f7a1ab0d5c
homework_1sem <st129909@student.spbu.ru>
2026-04-27 01:50:36 +0300

    feat: fixed the bugs

---
+++ NumberOfDegree/NumberOfDegree.fs
@@ -0,0 +1,15 @@
+﻿module NumberOfDegree
+
+let powersOfTwo n m =
+    if m < 0 then
+        Error "m must be non-negative"
+    else
+        let start = 2.0 ** float n
+
+        let rec loop count acc =
+            if count = 0 then
+                List.rev acc
+            else
+                loop (count - 1) ((List.head acc * 2.0) :: acc)
+
+        Ok (loop m [start])
